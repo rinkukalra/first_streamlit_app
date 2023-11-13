0b4bcab470b1d855b0a35cd674599a2b150d73c4
rinkukalra <150637910+rinkukalra@users.noreply.github.com>
2023-11-13 08:20:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title("My parents new healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 Oatmeal")
+streamlit.text("hardboiled Egg")
+streamlit.text("Spinach Smoothie")
+
