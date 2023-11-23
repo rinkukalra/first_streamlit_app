f948984f5c8cf23de7f277645c8d33c4d5981250
rinkukalra <150637910+rinkukalra@users.noreply.github.com>
2023-11-23 06:45:42 +0530

    Update streamlit_app.py
    insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,14 @@ streamlit.dataframe(fruityvice_normalized)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+
 my_data_rows = my_cur.fetchall()
+
 streamlit.text("Fruit List from Snowflake:")
 streamlit.dataframe(my_data_rows)
 add_fruit = streamlit.text_input('Which fruit you will like to add?','Jackfruit')
 streamlit.write('Thanks for adding ',add_fruit)
+my_cur.execute("insert into  pc_rivery_db.public.fruit_load_list values ('from streamlit')")
 
 
 
